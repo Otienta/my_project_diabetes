e264dc47ad3d88305022cc0d2f82cf8c927d4448
Tienta55 <ousmanetienta55@gmail.com>
2025-04-23 01:28:57 +0100

    Fix README conflict

--- app.py
+++ app.py
@@ -230,8 +230,8 @@ def logout():
     flash('Logged out successfully.', 'success')
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
 
 import os
 
